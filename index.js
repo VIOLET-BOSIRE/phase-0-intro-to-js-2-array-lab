b36d75cca13532b32c9b96832b348c804da63865
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-11-29 13:46:17 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+ cats.push("Ralph");
+ const copyOfCats=[...cats]
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+    const copyOfCats =[...cats]
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    const copyOfCats =[...cats]
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    const copyOfCats =[...cats]
+}
+function appendCat(names)
+{
+    return cats.concat(names)
+}
+function prependCat(name){
+    return[name,...cats]
+}
+function removeLastCat(){
+    return cats.slice(0,cats.length-1)
+}
+function removeFirstCat(){
+    return cats.slice(1,cats.length)
+}
